013f5d2824af3d872f76dca2f8414fe747711e68
BeriKotelko <berenika@kotelko.com>
2022-01-24 12:43:06 +0000

    styling acceptable and router set up for the death page and the home page, tdd'd.FLT.

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,22 +1,36 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import DeathScenario from "./components/DeathScenario";
 import "@testing-library/jest-dom/extend-expect";
 it("should call setCurrentUser when onclick", () => {
-    render( <App />
-    );
-    const headerText = screen.getByTestId("header");
-    expect(headerText).toHaveTextContent("Welcome to the Sherlock Game");
-  });
+  render(<HomePage />);
+  const headerText = screen.getByTestId("header");
+  expect(headerText).toHaveTextContent("Welcome to the Sherlock Game");
+});
 
-  it("should have a pick bottle 1 button", () => {
-    render( <App />
-    );
-    const headerText = screen.getByTestId("bottle1Button");
-    expect(headerText).toHaveTextContent("Drink from bottle 1");
-  });
-  it("should have a pick bottle 2 button", () => {
-    render( <App />
-    );
-    const headerText = screen.getByTestId("bottle2Button");
-    expect(headerText).toHaveTextContent("Drink from bottle 2");
-  });
+it("should have a pick bottle 1 button", () => {
+  render(<HomePage />);
+  const headerText = screen.getByTestId("bottle1Button");
+  expect(headerText).toHaveTextContent("Drink from bottle 1");
+});
+it("should have a pick bottle 2 button", () => {
+  render(
+    <Router>
+      {" "}
+      <HomePage />{" "}
+    </Router>
+  );
+  const headerText = screen.getByTestId("bottle2Button");
+  expect(headerText).toHaveTextContent("Drink from bottle 2");
+});
+
+it("should render the Death Scenario page", () => {
+  render(
+    <Router>
+      <DeathScenario />
+    </Router>
+  );
+  const deathGreeting = screen.getByTestId("deathGreeting");
+  expect(deathGreeting).toHaveTextContent("You died, sorry!");
+});
---
+++ my-app/src/components/DeathScenario.tsx
@@ -0,0 +1,3 @@
+export default function DeathScenario() {
+  return <h1 data-testid="deathGreeting">You died, sorry!</h1>;
+}
